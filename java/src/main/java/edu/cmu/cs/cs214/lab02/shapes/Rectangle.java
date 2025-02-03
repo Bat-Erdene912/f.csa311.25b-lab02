a34db4b1cc0bd6785db16fde7ee73b2d0373d839
Bat-Erdene912 <erdenee0912@gmail.com>
2025-02-04 00:38:33 +0800

    Update Rectangle.java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,15 +1,21 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
-    
-    public Rectangle(double height, double width){
-        this.height = height;
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
         this.width = width;
+        this.height = height;
     }
 
+    @Override
     public double getArea() {
-        return height * width;
+        return width * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle with width: " + width + " and height: " + height);
     }
 }
